b456eedd0773e154285aa5c0102252548b88f312
netusil <m.netusil@mikrovox.cz>
2023-10-22 13:01:17 +0200

    Calendar
    Updating only once a day at 00:00:01

--- widgets/calendar.py
+++ widgets/calendar.py
@@ -3,8 +3,9 @@ import datetime as dt
 
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
-from kivy.properties import ObjectProperty
 from kivy.clock import Clock
+from kivy.properties import ObjectProperty
+from kivy.logger import Logger
 
 
 class CalendarWidget(BoxLayout):
@@ -15,14 +16,37 @@ class CalendarWidget(BoxLayout):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        Clock.schedule_interval(self.update_day, 1.)
+        Clock.schedule_once(self.update, 1.)
 
-    def update_day(self, delta):
+    def update(self, delta=0):
+        self._update_day()
+        self._schedule_update()
+
+    def _update_day(self):
+        Logger.info('UPDATING CALENDAR ...')
         today = dt.date.today()
         self.day_num.text = today.strftime('%d')
         self.day_of_week.text = today.strftime('%A')
         self.month.text = today.strftime('%B %Y')
-        self.month_cal.text = calendar.month(today.year, today.month).split('\n', 1)[1]
+        self.month_cal.text = calendar\
+            .month(today.year, today.month)\
+            .split('\n', 1)[1]
+
+    def _schedule_update(self):
+        next_update = self._next_update()
+        next_in_secs = (next_update - dt.datetime.now()).total_seconds()
+        Logger.info(
+            f'NEXT CALENDAR UPDATE AT {next_update} O\'CLOCK, '
+            f'IN {next_in_secs} SECONDS.'
+        )
+        Clock.schedule_once(self.update, next_in_secs)
+
+    @staticmethod
+    def _next_update() -> dt.datetime:
+        now = dt.datetime.now()
+        next_update = now.replace(hour=0, minute=0, second=1) + \
+                      dt.timedelta(days=1)
+        return next_update
 
 
 class MonthWidget(Label):
