1fba947c6d97958cc678ca9a657f289d1c8533fb
Marek Netusil <marek.netusil@gmail.com>
2024-03-23 13:49:32 +0100

    ForecastWidget
    ditched the graph for a simpler icon based view

---
+++ weather/icons.py
@@ -0,0 +1,4 @@
+
+icons_list = {u'01d':u'B',u'01n':u'C',u'02d':u'H',u'02n':u'I',u'03d':u'N',u'03n':u'N',
+        u'04d':u'Y',u'04n':u'Y',u'09d':u'R',u'09n':u'R',u'10d':u'R',u'10n':u'R',u'11d':u'P',
+        u'11n':u'P',u'13d':u'W',u'13n':u'W',u'50d':u'M',u'50n':u'W'}
